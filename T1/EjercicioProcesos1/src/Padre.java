4b1f5deab3180506d2a4e72b91249a2fcedbaa49
Kay <katherine.agualongo@cesjuanpablosegundo.es>
2022-10-26 10:03:31 +0200

    update

--- T1/EjercicioProcesos1/src/Padre.java
+++ T1/EjercicioProcesos1/src/Padre.java
@@ -2,12 +2,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 public class Padre {
 
     public static void main(String args[]) {
 
         String line;
+        Cliente cliente1 = new Cliente(01,"1234X","Naiara","Gonzalez",22,600999888,"naiaragonzalez@gmail.com");
+        Cliente cliente2 = new Cliente(02,"1234Y","Juan","Manizales",26,600999777,"juanmanizales@gmail.com");
+        Cliente cliente3 = new Cliente(03,"1234Z","Angel","Castro",20,600999666,"angelcastro@gmail.com");
+        Cliente cliente4 = new Cliente(04,"1234A","Ricardo","Sangronis",18,600999555,"ricardosangronis@gmail.com");
+        Cliente cliente5 = new Cliente(05,"1234B","Angelica","Silvestre",22,600999444,"angelicasilvestre@gmail.com");
+        ArrayList<Cliente> listaClientes  = new ArrayList<>();
+
 
         try {
 
@@ -38,5 +46,87 @@ public class Padre {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+}
+
+class Cliente{
+    int id, telefono,edad;
+    String nombre,apellido,dni,correo;
+
+    public Cliente(){}
+    public Cliente(int id, String dni, String nombre, String apellido, int edad, int telefono, String correo) {
+        this.id = id;
+        this.telefono = telefono;
+        this.edad = edad;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.correo = correo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void mostrarDatos(){
+        System.out.println("ID: " + id + "\n" +
+                "DNI: " + dni + "\n" +
+                "Nombre: " + nombre + "\n" +
+                "Apellido: " + apellido + "\n" +
+                "Edad: " + edad + "\n" +
+                "Telefono: : " + telefono + "\n" +
+                "Correo: " + correo + "\n");
     }
 }
